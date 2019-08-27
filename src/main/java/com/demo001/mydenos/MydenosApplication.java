141507beab1a98859d442ea4260f0655ac0250e6
my-java <myqq@qq.com>
2019-08-27 16:05:24 +0800

    Initial commit

---
+++ src/main/java/com/demo001/mydenos/MydenosApplication.java
@@ -0,0 +1,13 @@
+package com.demo001.mydenos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MydenosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MydenosApplication.class, args);
+    }
+
+}
